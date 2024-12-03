46095ae766aa112d2ec1eff5ced311a878b9eda6
classyoon <96508736+classyoon@users.noreply.github.com>
2024-12-03 11:50:56 -0500

    Initial Commit

---
+++ ShopKeeperSimTests/ShopKeeperSimTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShopKeeperSimTests.swift
+//  ShopKeeperSimTests
+//
+//  Created by Conner Yoon on 12/3/24.
+//
+
+import Testing
+@testable import ShopKeeperSim
+
+struct ShopKeeperSimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
